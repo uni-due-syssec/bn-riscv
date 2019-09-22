de9ee93c7788c688d335047d9e885643e6f9c489
Katharina Utz <katharina.utz@stud.uni-due.de>
2019-09-22 18:04:11 +0200

    overcome problems with initialising ELF architecture

--- __init__.py
+++ __init__.py
@@ -1,4 +1,10 @@
 from .riscv import RISCV, RISCVView
+from binaryninja import architecture, binaryview, enums
 
 RISCV.register()
 RISCVView.register()
+
+arch = architecture.Architecture['riscv']
+binaryview.BinaryViewType['ELF'].register_arch(
+    243, enums.Endianness.LittleEndian, arch
+)
