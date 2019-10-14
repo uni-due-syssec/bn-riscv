f4a809530b4bf30640ea068f957f50f35a83f069
Katharina Utz <katharina.utz@stud.uni-due.de>
2019-10-14 11:57:38 +0200

    included instruction types

--- instruction.py
+++ instruction.py
@@ -1,26 +1,37 @@
 from capstone import *
+from capstone.riscv import *
 from collections import namedtuple
 
 
 def decode(data, addr):
 
-    instr = namedtuple('Instruction', 'size name operand')
+    instr = namedtuple('Instruction', 'size name op imm')
     size = 2
     name = ""
-    operand = ""
+    op = ""
+    imm = 0
 
-    md = Cs(CS_ARCH_RISCV, CS_MODE_RISCV64)
-    for i in md.disasm(data, addr):
-        size = i.size
-        name = i.mnemonic
-        operand = i.op_str
-        break
+    md = Cs(CS_ARCH_RISCV, CS_MODE_RISCV32)
+    md.detail = True
 
-    if not name:
-        if data[:2] == b'\x82\x80':
-            name = "ret"
+    for insn in md.disasm(data, addr):
+        size = insn.size
+        name = insn.mnemonic
 
-    return instr(size, name, operand)
+        if len(insn.operands) > 0:
+            for i in insn.operands:
+                if i.type == RISCV_OP_REG:
+                    op += " " + (insn.reg_name(i.value.reg))
+                elif i.type == RISCV_OP_IMM:
+                    imm = i.value.imm
+                elif i.type == RISCV_OP_MEM:
+                    if i.mem.base != 0:
+                        op += " " + insn.reg_name(i.mem.base)
+                    if i.mem.disp != 0:
+                        imm = i.mem.disp
+
+        return instr(size, name, op, imm)
+
 
 
 
