1b89b73abfef4273d5f6b45bb265d548876562a6
Katharina Utz <katharina.utz@stud.uni-due.de>
2019-10-18 19:21:27 +0200

    added calling convention

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
-from .riscv import RISCV
 from binaryninja import architecture, binaryview, enums
+from .riscv import RISCV
+from .const import DefaultCallingConvention
 
 RISCV.register()
 
 arch = architecture.Architecture['riscv']
+arch.register_calling_convention(DefaultCallingConvention(arch, 'default'))
+arch.standalone_platform.default_calling_convention = arch.calling_conventions['default']
 binaryview.BinaryViewType['ELF'].register_arch(
     243, enums.Endianness.LittleEndian, arch
 )
