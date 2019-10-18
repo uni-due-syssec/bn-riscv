eac4fd353d5fb8fc9547a6038f0a2ebda139b2b1
Katharina Utz <katharina.utz@stud.uni-due.de>
2019-10-18 19:22:57 +0200

    calling convention

--- const.py
+++ const.py
@@ -1,2 +1,12 @@
+from binaryninja import CallingConvention
+
 ADDR_SIZE = 4
 INT_SIZE = 4
+
+
+class DefaultCallingConvention(CallingConvention):
+    name = "default"
+    int_arg_regs = ['a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7']
+    int_return_reg = 'a0'
+    high_int_return_reg = 'a1'
+
