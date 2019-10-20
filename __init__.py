ce8f428945590765d9a849814784bd1eaba17e9e
Katharina Utz <katharina.utz@stud.uni-due.de>
2019-10-20 15:26:25 +0200

    risc-v 64bit support

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 from binaryninja import architecture, binaryview, enums
-from .riscv import RISCV
-from .const import DefaultCallingConvention
+from .riscv import RISCV, RISCV64
+from .calling_convention import DefaultCallingConvention
 
 RISCV.register()
+RISCV64.register()
 
 arch = architecture.Architecture['riscv']
 arch.register_calling_convention(DefaultCallingConvention(arch, 'default'))
