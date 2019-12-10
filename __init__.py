b4e39c981351a9b330300ff585cfaecaaa9b62f6
Katharina Utz <katharina.utz@stud.uni-due.de>
2019-12-10 10:14:41 +0100

    Replace __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from binaryninja import architecture, binaryview, enums
-from .riscv import RISCV, RISCV64
+from .riscv import RISCV
 from .calling_convention import DefaultCallingConvention
 
 RISCV.register()
@@ -10,13 +10,3 @@ arch.standalone_platform.default_calling_convention = arch.calling_conventions['
 binaryview.BinaryViewType['ELF'].register_arch(
     243, enums.Endianness.LittleEndian, arch
 )
-
-RISCV64.register()
-
-arch64 = architecture.Architecture['riscv64']
-arch64.register_calling_convention(DefaultCallingConvention(arch64, 'default'))
-arch64.standalone_platform.default_calling_convention = arch64.calling_conventions['default']
-binaryview.BinaryViewType['ELF'].register_arch(
-    243, enums.Endianness.LittleEndian, arch64
-)
-
